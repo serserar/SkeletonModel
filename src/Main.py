2028294fdedb53ad53d20bf8a7421f0e068794e5
Sergio Serrano <serserar@gmail.com>
2018-09-01 01:21:30 +0200

    fix download dataset

--- src/Main.py
+++ src/Main.py
@@ -22,11 +22,9 @@ from os import path
 import matplotlib.pyplot as plt
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from google.colab import auth
 from oauth2client.client import GoogleCredentials
 
 def downloadDatasetFromDrive(datasetId, download_dir):
-    auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
     drive = GoogleDrive(gauth)
