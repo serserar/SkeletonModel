55aa226c1d307f4cb55e462be62d8ba7aa329904
Sergio <serserar@gmail.com>
2018-09-20 11:23:45 +0200

    mod model

--- src/Main.py
+++ src/Main.py
@@ -183,9 +183,9 @@ def buildEncoder(model,filters,ishape=0):
 
 def buildEncoder3d(model,filters,filtersize=3,ishape=0):
     if (ishape!=0):
-        model.add(Conv3D(filters, (filtersize, filtersize, filtersize), padding='same',input_shape=ishape))
+        model.add(Conv3D(filters, (filtersize, filtersize, filtersize),activation='relu', padding='same',input_shape=ishape))
     else:
-        model.add(Conv3D(filters, (filtersize, filtersize, filtersize), padding='same'))
+        model.add(Conv3D(filters, (filtersize, filtersize, filtersize),activation='relu', padding='same'))
     #model.add(BN())
     #model.add(GN(0.3))    
     model.add(MaxPooling3D((2, 2, 2), padding='same'))
